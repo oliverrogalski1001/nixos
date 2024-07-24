247951b41a7757d064b7c183a4e66d657e8d3ac7
Oliver Rogalski <oliverrogalski1001@gmail.com>
2024-07-24 17:29:07 -0500

    remove steam

--- configuration.nix
+++ configuration.nix
@@ -149,12 +149,12 @@
     NIXOS_OZONE_WL = "1";
   };
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  # };
 
 
   # hyprland stuff
