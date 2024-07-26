b7ee0766566c9a3ba4d183abb209dbe94164da2b
Oliver Rogalski <oliverrogalski1001@gmail.com>
2024-07-26 02:46:14 -0500

    wallpaper script

--- dotfiles/.config/rofi/scripts/wallpaper.sh
+++ dotfiles/.config/rofi/scripts/wallpaper.sh
@@ -4,34 +4,13 @@ wallDIR="${HOME}/Pictures/wallpapers/"
 FPS=60
 TYPE="any"
 DURATION=2
-#BEZIER=".43,1.19,1,.4"
 SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION"
-#PICS=($(fd . "${wallDIR}"))
-PICS=($(find "${wallDIR}" -type f \( -iname \*.jpg -o -iname \*.jpeg -o -iname \*.png -o -iname \*.gif \)))
-ROFI="rofi -i -show -dmenu"
-
-menu() {
-  sorted_options=($(printf '%s\n' "${PICS[@]}" | sort))
-  for pic_path in "${sorted_options[@]}"; do
-    pic_name=$(basename "$pic_path")
-    printf "%s/n" "$pic_name"
-  done
-}
 
 main() {
-  choice=$(menu | ${ROFI})
+  choice=$(fd . "${wallDIR}" | xargs basename -a | rofi -i -show -dmenu)
 
-  pic_index=-1
-  for i in "${!PICS[@]}"; do
-    filename=$(basename "${PICS[$i]}")
-    if [[ "$filename" == "$choice"* ]]; then
-      pic_index=$i
-      break
-    fi
-  done
-
-  if [[ $pic_index -ne -1 ]]; then
-    swww img -o "${PICS[$pic_index]}" $SWWW_PARAMS
+  if [[ -n $choice ]]; then
+    swww img "$wallDIR$choice" $SWWW_PARAMS
   else
     echo "Image not found."
     exit 1
@@ -39,5 +18,3 @@ main() {
 }
 
 main
-
-sleep 0.5
